bb50780b7167d4c860d99e57734c6fd74560e109
d-horowitz <d0556776910@outlook.co.il>
2023-12-29 02:04:27 +0200

    1.7

--- MyClinicServer/Migrations/20231204153623_mssql.local_migration_274.cs
+++ MyClinicServer/Migrations/20231204153623_mssql.local_migration_274.cs
@@ -15,8 +15,7 @@ namespace MyClinicServer.Migrations
                 name: "Patient",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<int>(type: "int", nullable: false),
                     Gender = table.Column<int>(type: "int", nullable: false),
                     Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
